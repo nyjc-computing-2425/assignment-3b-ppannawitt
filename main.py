11232ab0a200ebd863fe3335d46c759f968be9d5
ppannawitt <106550775+ppannawitt@users.noreply.github.com>
2024-03-19 06:26:33 +0000

    submit

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 stdform = input('Enter a number in scientific notation: ')
 stdform = stdform.strip()
-
+idx = stdform.find('x')
+base = str(float(stdform[0:idx]))
+idx = stdform.find('^')
+power = stdform[idx+1:]
+print("This number in E notation is " + base + 'E' + power + '.')
 # Type your code below
